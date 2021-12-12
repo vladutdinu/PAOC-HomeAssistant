6ab059b213ec83f0fed826f4df82b8a2d8708537
VladutDinu <48055925+VladutDinu@users.noreply.github.com>
2021-12-12 19:15:26 +0000

    api leds

--- voice_recon/backend/lights.py
+++ voice_recon/backend/lights.py
@@ -6,30 +6,30 @@ GPIO.setup(18, GPIO.OUT) #18, 23, 25, 20
 GPIO.setup(20, GPIO.OUT) 
 GPIO.setup(26, GPIO.OUT) 
 GPIO.setup(25, GPIO.OUT) 
-def bedroom():
-    print("Led on")
-    GPIO.output(26, GPIO.HIGH)
+def bedroom(on):
+    if on == 1:
+        print("Led on")
+        GPIO.output(26, GPIO.HIGH)
+    elif on != 1:
+        print("Led off")
+        GPIO.output(26, GPIO.LOW)
 
-    time.sleep(2)
-    print("Led off")
-    GPIO.output(26, GPIO.LOW)
-
-def living_room():
-    print("Led on")
-    GPIO.output(25, GPIO.HIGH)
-
-    time.sleep(2)
-    print("Led off")
-    GPIO.output(25, GPIO.LOW)
+def living_room(on):
+    if on == 1:
+        print("Led on")
+        GPIO.output(25, GPIO.HIGH)
+    elif on != 1:
+        print("Led off")
+        GPIO.output(25, GPIO.LOW)
 
 
-def garden():
-    print("Led on")
-    GPIO.output(20, GPIO.HIGH)
-
-    time.sleep(2)
-    print("Led off")
-    GPIO.output(20, GPIO.LOW)
+def garden(on):
+    if on == 1:
+        print("Led on")
+        GPIO.output(20, GPIO.HIGH)
+    elif on != 1:
+        print("Led off")
+        GPIO.output(20, GPIO.LOW)
 
 def wind():
     print("motor on")
--- voice_recon/backend/main.py
+++ voice_recon/backend/main.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import jsonify, make_response
 from flask_cors import CORS
-from lights import led
+import lights
+import requests
 from DHT11Test import temp
 from datetime import datetime
 from datetime import datetime
@@ -13,41 +14,54 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 token = "00MG5PQI2xwnFLLPfboOcXvQhOls-Y698AKDn8zHN7JG1kno3onDihW6ywc8LPo9oLOiTMvSGBe0Uwu_vcFpEw=="
 org = "astroswipe2021@gmail.com"
 bucket = "astroswipe2021's Bucket"
+ESP_IP="http://192.168.43.47"
 app = Flask(__name__)
 CORS(app)
 @app.route("/")
 def hello_word():
     return "<p>pwp</p>"
 
-@app.route("/lights")
-def lights():
-    led()
+@app.route("/led1/<on>", methods=['POST'])
+def bedroom(on):
+    lights.bedroom(float(on))
     return "OK"
 
+@app.route("/led2/<on>", methods=['POST'])
+def livingroom(on):
+    lights.living_room(float(on))
+    return "OK"
+
+@app.route("/led3/<on>", methods=['POST'])
+def garden(on):
+    lights.garden(float(on))
+    return "OK"
 
 @app.route("/temps")
 def temps():
-	temperature, humidity = temp()
-	with InfluxDBClient(url="https://eu-central-1-1.aws.cloud2.influxdata.com", token=token, org=org) as client:
+	temperature = requests.get(ESP_IP+"/temp").content.decode('utf-8')
+	print(temperature)
+	#temperature, humidity = temp()
+	with InfluxDBClient(url="https://eu-central-1-1.aws.cloud2.influxdata.com", token=token, org=org, verify_ssl=False) as client:
 		write_api = client.write_api(write_options=SYNCHRONOUS)
 		#data="pi4,host=pi4 temp={} humidity={}".format(temperature, humidity)
-		point = Point("mem").tag("host", "pi4").field("temp", temperature).time(datetime.utcnow(), WritePrecision.NS)
+		point = Point("mem").tag("host", "pi4").field("temp", float(temperature)).time(datetime.utcnow(), WritePrecision.NS)
 		write_api.write(bucket, org, point)
 		return make_response(jsonify({
-			"temp": temperature,
+			"temp": float(temperature),
 			"time": datetime.now().strftime('%H:%M:%S')
 			}),200)
 
 @app.route("/humds")
 def humds():
-	temperature, humidity = temp()
-	with InfluxDBClient(url="https://eu-central-1-1.aws.cloud2.influxdata.com", token=token, org=org) as client:
+	humidity = requests.get(ESP_IP+"/humd").content.decode('utf-8')
+	print(humidity)
+	with InfluxDBClient(url="https://eu-central-1-1.aws.cloud2.influxdata.com", token=token, org=org,  verify_ssl=False) as client:
 		write_api = client.write_api(write_options=SYNCHRONOUS)
 		#data="pi4,host=pi4 temp={} humidity={}".format(temperature, humidity)
-		point = Point("mem").tag("host", "pi4").field("humidity", humidity).time(datetime.utcnow(), WritePrecision.NS)
+		point = Point("mem").tag("host", "pi4").field("humidity", float(humidity)).time(datetime.utcnow(), WritePrecision.NS)
 		write_api.write(bucket, org, point)
 		return make_response(jsonify({
-			"humidity": humidity,
+			"humidity": float(humidity),
 			"time": datetime.now().strftime('%H:%M:%S')
 			}),200)
 @app.route("/temperaturi")
@@ -56,7 +70,7 @@ def temperaturi():
 		"temp": [],
 		"time": []
 	}
-	with InfluxDBClient(url="https://eu-central-1-1.aws.cloud2.influxdata.com", token=token, org=org) as client:
+	with InfluxDBClient(url="https://eu-central-1-1.aws.cloud2.influxdata.com", token=token, org=org,  verify_ssl=False) as client:
 		q4='''
 			from(bucket: "astroswipe2021's Bucket")
   |> range(start: -30d)
@@ -80,7 +94,7 @@ def umiditati():
 		"humidity": [],
 		"time": []
 	}
-	with InfluxDBClient(url="https://eu-central-1-1.aws.cloud2.influxdata.com", token=token, org=org) as client:
+	with InfluxDBClient(url="https://eu-central-1-1.aws.cloud2.influxdata.com", token=token, org=org, verify_ssl=False) as client:
 		q4='''
 			from(bucket: "astroswipe2021's Bucket")
   |> range(start: -30d)
